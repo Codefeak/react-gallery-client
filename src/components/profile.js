d40ee66c56d0bf718fa72fd21144b8a20525e20c
codefeak <codefever17@gmail.com>
2018-08-01 16:15:42 +0300

    changed profilejs to class

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LeftPannel from './LeftPannel';
 import MidPannel from './MidPannel';
 import RightPannel from './RightPannel';
 
-const Profile = (props)=>{
+class Profile extends Component{
 
     componentDidMount=()=>{
         fetch('https://react-gallery-server.herokuapp.com/api')
         .then(res=> console.log(res))
     }
 
-    console.log(props.location);
-    // const username = props.location.state.some.username.toUpperCase();
-    return(
-        <div>
-            <h1>Profile Page</h1>
-            <h2>Welcome Back</h2>
-            <div className="main-container">
-                <div className="left-container">
-                    <LeftPannel data="" classShow=""/>
+    render(){
+        return(
+            <div>
+                <h1>Profile Page</h1>
+                <h2>Welcome Back</h2>
+                <div className="main-container">
+                    <div className="left-container">
+                        <LeftPannel data="" classShow=""/>
+                    </div>
+                    {/* <div className="right-container">
+                        <RightPannel data="" classShow=""/>
+                    </div>
+                    <div className="mid-container">
+                        <MidPannel data="" click=""/>
+                    </div> */}
                 </div>
-                {/* <div className="right-container">
-                    <RightPannel data="" classShow=""/>
-                </div>
-                <div className="mid-container">
-                    <MidPannel data="" click=""/>
-                </div> */}
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Profile;

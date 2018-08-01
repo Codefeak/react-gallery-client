0d7b59c15acabdb8b38a192384fe1638f5a265f6
codefeak <codefever17@gmail.com>
2018-08-01 16:35:16 +0300

    changed profilejs to class

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LeftPannel from './LeftPannel';
+import axios from 'axios';
 import MidPannel from './MidPannel';
 import RightPannel from './RightPannel';
 

d5fd7a2a153534e5a7bb89dd003e1c25b427722b
codefeak <codefever17@gmail.com>
2018-08-02 14:29:44 +0300

    link to api added

--- src/components/RightPannel.js
+++ src/components/RightPannel.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import axios from 'axios';
+import { withRouter } from 'react-router';
 import digia from './images/digia-logo.svg';
 import druid from './images/druid-logo.svg';
 import fraktio from './images/fraktio-logo-white.svg';
@@ -6,13 +8,20 @@ import wunder from './images/wunder-logo.png';
 import omni from './images/omni-logo.png';
 import helsinki from './images/Helsinki-logo-2017.png';
 
-const RightPannel=(props)=>{
+class RightPannel extends Component{
+
+		Click=(e)=>{
+			e.preventDefault;
+			this.props.history.replace('https://react-gallery-server.herokuapp.com/api')
+		}
+		
+		render(){
 		return(
 			<div>
-				<div className={`info ${props.classShow}`}>
+				<div className={`info ${this.props.classShow}`}>
 					<div className="right-headers">
 						<h1 id="partners">Partners</h1>
-						<h1 id="api">API</h1>
+						<h1 id="api"><a onClick={this.Click}>API</a></h1>
 						<h1 id="add-new">AddNew</h1>
 					</div>
 					<div className="img-logos">
@@ -26,24 +35,25 @@ const RightPannel=(props)=>{
 				</div>
 				
 
-				<div className={`indiv-information ${props.classShow==="show"?"hide":"show"}`}>
-					<input className={`indiv-information ${props.classShow==="show"?"hide":"show"}`} type="button" value="X" id="back" onClick={props.click}/> 
+				<div className={`indiv-information ${this.props.classShow==="show"?"hide":"show"}`}>
+					<input className={`indiv-information ${this.props.classShow==="show"?"hide":"show"}`} type="button" value="X" id="back" onClick={this.props.click}/> 
 					<div className = "info-div ">	
-						<h2 className="key-name">Name: </h2><p>{props.data[0] && `${props.data[0].firstName} ${props.data[0].lastName}`}</p>
-						<h2 className="key-name">Title: </h2><p>{props.data[0] && `${props.data[0].title}`}</p>
-						<h2 className="key-name">Nationality: </h2><p>{props.data[0] && `${props.data[0].nationality}`}</p>
-						<h2 className="key-name">Skills: </h2><p>{props.data[0] && `${props.data[0].skills.join(", ")}`}</p>
-						<h2 className="key-name">Why: </h2><p>{props.data[0] && `${props.data[0].whySofterDeveloper}`}</p>
-						<h2 className="key-name">Vision: </h2><p>{props.data[0] && `${props.data[0].longTermVision}`}</p>
-						<h2 className="key-name">Motivation: </h2><p>{props.data[0] && `${props.data[0].motivatesMe}`}</p>
-						<h2 className="key-name">Favorite Quote: </h2><p>{props.data[0] && `${props.data[0].favoriteQuote}`}</p>
-						<h2 className="key-name">Joined Date: </h2><p>{props.data[0] && `${props.data[0].joinedOn}`}</p>
+						<h2 className="key-name">Name: </h2><p>{this.props.data[0] && `${this.props.data[0].firstName} ${this.props.data[0].lastName}`}</p>
+						<h2 className="key-name">Title: </h2><p>{this.props.data[0] && `${this.props.data[0].title}`}</p>
+						<h2 className="key-name">Nationality: </h2><p>{this.props.data[0] && `${this.props.data[0].nationality}`}</p>
+						<h2 className="key-name">Skills: </h2><p>{this.props.data[0] && `${this.props.data[0].skills.join(", ")}`}</p>
+						<h2 className="key-name">Why: </h2><p>{this.props.data[0] && `${this.props.data[0].whySofterDeveloper}`}</p>
+						<h2 className="key-name">Vision: </h2><p>{this.props.data[0] && `${this.props.data[0].longTermVision}`}</p>
+						<h2 className="key-name">Motivation: </h2><p>{this.props.data[0] && `${this.props.data[0].motivatesMe}`}</p>
+						<h2 className="key-name">Favorite Quote: </h2><p>{this.props.data[0] && `${this.props.data[0].favoriteQuote}`}</p>
+						<h2 className="key-name">Joined Date: </h2><p>{this.props.data[0] && `${this.props.data[0].joinedOn}`}</p>
 					</div>
 				</div>
 
 			</div>
 			
 		);
+	}
 }
 
-export default RightPannel;
+export default withRouter(RightPannel);
